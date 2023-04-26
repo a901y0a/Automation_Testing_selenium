fb9f37e88deaf6956ab0c901c3f64d1a81c3736f
Agalya <113664802+a901y0a@users.noreply.github.com>
2023-04-26 09:28:25 +0530

    Add files via upload

---
+++ Selenium-DAY 4/newproject/src/test/java/com/Pack/OneClass.java
@@ -0,0 +1,21 @@
+package com.Pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OneClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(co);
+		driver.get("https://www.google.com");
+
+	}
+
+}
